110bb093fdd4f4cf8e2613e67c97bfe28bf877a1
Chemah <chemah.hack@gmail.com>
2017-02-07 17:30:24 -0600

    Cambie la ruta de clase de conexión por la del servidor -Roller

--- models/General/cls_Mod_General.php
+++ models/General/cls_Mod_General.php
@@ -1,6 +1,7 @@
 <?php
     $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-    require_once "$root/SGAC_v1.0/trunk/models/MySQL/MySQL.php";
+    require_once "187.217.117.12/MySQL.php"
+    //require_once "$root/SGAC_v1.0/trunk/models/MySQL/MySQL.php";
     
     class cls_Mod_General {
         #VARIABLES GLOBALES
